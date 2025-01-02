ef0ee44495a340c69dce029a84b7afff10cc4eb2
LD2343 <logandaniel2343@gmail.com>
2025-01-02 16:47:00 -0700

    Update A-backend.tf

--- Tokyo/A-backend.tf
+++ Tokyo/A-backend.tf
@@ -1,7 +1,7 @@
 /*terraform {
     backend "s3" {
-        bucket = "tokyo-s3-backend01"
-        key = "ToykoLinux"
-        region = "ap-northeast-1"      
+        bucket = "mylocalterraformecs"
+        key = "MyLinuxBox"
+        region = "us-east-1"      
 }
 }*/
